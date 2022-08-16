0c1db0bd38734924b37e7e4c75db7bf94e984c94
User <voditel.vlad@yandex.ru>
2022-08-16 10:08:36 +0300

    add template

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,3 +3,12 @@
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
+
+
+const middleware = (func) => (req, res) => {
+
+  if (req.body === null && req.method === 'POST')
+    throw new Error("Can't divide by zero")
+  console.log(req)
+  return func(req, res)
+}
