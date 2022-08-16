428bc8f78f56f5b2f87ae26fc818df2fb31d9829
User <voditel.vlad@yandex.ru>
2022-08-16 11:23:15 +0300

    add middleware

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,14 +1,39 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import jwt from 'jsonwebtoken';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+
+
+
+const middleware = cb => (req, res) => {
+
+  if (req.query.test === 'aboba') {
+    console.log('authorised')
+    return cb(req, res, {isAuthorized: true})
+  } else {
+    console.log('not authorised')
+    return cb(req, res, {isAuthorized: false})
+  }
 }
 
 
-const middleware = (func) => (req, res) => {
+export default middleware(async (req, res, accountability) => {
 
-  if (req.body === null && req.method === 'POST')
-    throw new Error("Can't divide by zero")
-  console.log(req)
-  return func(req, res)
-}
+  if (req.query.sign) {
+    try {
+      const decoded = jwt.verify(req.query.sign, 'key');
+      console.log('decoded ', decoded)
+      //user id return
+      res.status(200).json({body: decoded.userId})
+    } catch (e) {
+      res.status(500).json({error: 'Not valid key!'})
+    }
+  } else {
+    res.status(500).json({error: 'No sign!'})
+  }
+})
