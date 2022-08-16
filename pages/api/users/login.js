14515a96d42803196818daeb6f500c10a85802e0
Артём Жидков <artemzidkov385@gmail.com>
2022-08-16 11:34:08 +0300

    2

---
+++ pages/api/users/login.js
@@ -0,0 +1,17 @@
+export default function handler(req, res) {
+    const {method} = req;
+    if (method === 'POST') {
+        const body = JSON.parse(req.body);
+        if (body.username && body.password) {
+            try {
+                let user = login(body.username, body.password);
+                res.status(200).json(user);
+            }
+            catch (e) {
+                res.status(403).json({
+                    message: e.message
+                })
+            }
+        }
+    }
+}
