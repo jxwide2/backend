1a553c85da8ad76a424a1e21aa30cf361d9dbf85
Your Name <you@example.com>
2022-08-16 17:04:52 +0300

    11

--- components/db/dbclient/users_sessions/users_sessions-service.js
+++ components/db/dbclient/users_sessions/users_sessions-service.js
@@ -1,13 +1,23 @@
 import {database} from "../db";
 
 export async function createRelation(userId, sessionId, role = "member") {
-    await database('users_sessions').insert({
-        userId,
-        sessionId,
-        userRole: role
-    });
+    // await database('users_sessions').insert({
+    //     userId,
+    //     sessionId,
+    //     userRole: role
+    // });
+
+    await database.transaction(async (trx) => {
+        await trx('users_sessions').insert({
+            userId,
+            sessionId,
+            userRole: role
+        })
+    })
+
     return database('users_sessions')
         .where('userId', userId)
         .where('sessionId', sessionId)
+        .where('userRole', role)
         .first();
 }
