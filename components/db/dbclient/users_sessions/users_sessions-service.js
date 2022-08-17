a0afb0b71eb7e333e487fb0593b06ae433918ae1
Артём Жидков <artemzidkov385@gmail.com>
2022-08-17 16:30:24 +0300

    4

--- components/db/dbclient/users_sessions/users_sessions-service.js
+++ components/db/dbclient/users_sessions/users_sessions-service.js
@@ -1,19 +1,19 @@
 import {database} from "../db";
 
 export async function createRelation(userId, sessionId, role = "member") {
-    // await database('users_sessions').insert({
-    //     userId,
-    //     sessionId,
-    //     userRole: role
-    // });
+    await database('users_sessions').insert({
+        userId,
+        sessionId,
+        userRole: role
+    });
 
-    await database.transaction(async (trx) => {
-        await trx('users_sessions').insert({
-            userId,
-            sessionId,
-            userRole: role
-        })
-    })
+    // await database.transaction(async (trx) => {
+    //     await trx('users_sessions').insert({
+    //         userId,
+    //         sessionId,
+    //         userRole: role
+    //     })
+    // })
 
     return database('users_sessions')
         .where('userId', userId)
