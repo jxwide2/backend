e6ad4111c0ebdea5a8a23cfa2fbcb6cb4a18c190
User <voditel.vlad@yandex.ru>
2022-08-17 11:43:18 +0300

    renamed variables

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,11 +1,15 @@
-import {userCreate, userLogin} from "../../components/db/dbclient/users/user-service";
+import {userLogin} from "../../components/db/dbclient/users/user-service";
+import {middlewareHelper} from "../../lib/middleware";
+import {authMiddleware} from "../../lib/middleware/auth";
+import {validationMiddleware} from "../../lib/middleware/validation";
 
 
-export default async function handler(req, res) {
 
-    //если логин не в дб
+const auth = middlewareHelper([authMiddleware, validationMiddleware('user')], async (req, res) => {
     const user = await userLogin(req.body.username, req.body.password)
     console.log(user)
     res.status(user?.error ? 422 : 200).json(user)
+})
 
-}
+export default auth;
+
