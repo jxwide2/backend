83512cb46ad757bef859eb90066bcf0bd8e54777
User <voditel.vlad@yandex.ru>
2022-08-17 15:18:18 +0300

    wrapped index

--- pages/api/sessions/index.js
+++ pages/api/sessions/index.js
@@ -1,26 +1,18 @@
 import {sessionCreate, sessionGet} from "../../../components/db/dbclient/sessions/session-service";
+import {middlewareHelper} from "../../../lib/middleware";
+//import {validationMiddleware} from "../../../lib/middleware/validation";
 
-export default async function handler(req, res) {
+
+
+const sessions = middlewareHelper([authMiddleware], async (req, res) => {
     if (req.method === 'POST') {
-
-
-
-
-
-
         //создаем комнату
         const a = await sessionCreate(req.body)
-
         return res.status(200).json(a)
     } else {
-
         req.query.userid
         //смотрим бд
-
         const sessions = [1, 2, 3, 4]
-
         return res.status(200).json({sessions: sessions})
-
-
     }
-}
+})
