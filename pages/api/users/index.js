9018cd76cde317e97d681d5e5d96b80d524585ad
Артём Жидков <artemzidkov385@gmail.com>
2022-08-17 11:36:34 +0300

    4

--- pages/api/users/index.js
+++
@@ -1,6 +0,0 @@
-import {findAll} from "../../../components/db/dbclient/users/user-service";
-
-export default async function handler(req, res) {
-    let users = await findAll();
-    res.status(200).json(users);
-}
