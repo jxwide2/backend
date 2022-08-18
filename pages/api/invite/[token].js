f2ede6a5efd12fe32f331d512d406875d711c3cc
zig <mrigorgud@mail.ru>
2022-08-18 10:18:28 +0300

    da

--- pages/api/invite/[token].js
+++ pages/api/invite/[token].js
@@ -1,11 +1,20 @@
-export default function handler(req, res) {
+import jwt from "jsonwebtoken";
+import {middlewareHelper} from "../../../lib/middleware";
+import {authMiddleware} from "../../../lib/middleware/auth";
+import {createRelation} from "../../../components/db/dbclient/users_sessions/users_sessions-service";
+
+
+const invite = middlewareHelper([authMiddleware], async (req, res, context) => {
 
     //КТО ПРОЧИТАЛ ТОТ
 
-    req.query.token
+
+    const sessionId = await jwt.verify(req.query.token, process.env.SUPER_PRIVATE_KEY)
+    createRelation(context.id, sessionId)
 
 
-    return res.status(200).json({})
+    return res.status(200).json({id: sessionId})
 
 
-}
+})
+export default invite
